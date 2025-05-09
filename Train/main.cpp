f176af0bacb6eea861d352c9ef356c7f61e2e46b
Talentjoe <88471172+Talentjoe@users.noreply.github.com>
2025-05-09 12:07:56 -0700

    防止过拟合

--- Train/main.cpp
+++ Train/main.cpp
@@ -14,17 +14,19 @@ int main() {
     srand(time(nullptr) * 3049);
 
     auto *nn = new NN::NNcore();
-    nn->init(vector{28 * 28,300, 10}, Srate);
+    nn->init(vector{28 * 28,128, 10}, Srate);
 
     vector<vector<double> > inData;
     vector<int> outData;
     inData = readData::readData::readImageData("../Data/train-images.idx3-ubyte",10000);
     outData = readData::readData::readTagData("../Data/train-labels.idx1-ubyte", 10000);
 
+    for (int i = 0; i < 5; i++) {
     for (int j = 0; j < termsOfTrain; j++) {
         nn->train(inData, outData, true);
         //Srate *= 0.05;
         nn->changeStudyRate(Srate);
+    }
     }
 
     vector<vector<double> > testInData;
